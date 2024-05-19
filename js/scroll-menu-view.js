e65d5b54e68672fed3ec402c473cb7c67ae69a89
Gabriel Baltar <55629378+gabrielbaltar@users.noreply.github.com>
2024-05-19 19:46:18 -0300

    feat: adicionando funcionalidade scroll ao menu

---
+++ js/scroll-menu-view.js
@@ -0,0 +1,43 @@
+export default function hiddenMenu() {
+    
+    document.addEventListener("DOMContentLoaded", () => {
+
+    let lastScrollTop = window.scrollY || document.documentElement.scrollTop;
+    const menu = document.querySelector('[data-scroll="scroll-menu"]');
+
+    console.log(menu);
+
+    if(!menu) {
+        console.log("Elemento não encontrado");
+        return;
+    }
+
+    console.log('Elmento encontrado');
+
+    window.addEventListener("scroll", () => {
+
+        let scrollTop = window.scrollY|| document.documentElement.scrollTop;
+        console.log('ScrollTop: ', scrollTop);
+
+        if(scrollTop > lastScrollTop) {
+
+            //scroll para baixo remove o menu
+
+            console.log('Scroll para baixo');
+            menu.classList.add("hidden");
+
+        }else if(scrollTop < lastScrollTop){
+
+            //scroll para cima volta com o menu
+            console.log('Scroll para cima');
+            menu.classList.remove("hidden");
+
+        }
+
+        lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
+
+    }, false);
+
+});
+
+}
