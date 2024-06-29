be4b14a9b707719d67afd1eb9a17807e25a679fc
Gabriel Baltar <55629378+gabrielbaltar@users.noreply.github.com>
2024-06-29 02:27:09 -0300

    feat: add contact modal

---
+++ js/modal-contact.js
@@ -0,0 +1,27 @@
+export default function openModal() {
+    
+    const contactButton = document.querySelector('[data-contact-button]');
+    const contactModal = document.querySelector('[data-modal-contact]');
+    const buttonCloseModal = document.querySelector('[data-moda-close]');
+
+    /*console.log(contactButton);
+    console.log(contactModal);
+    console.log(buttonCloseModal);*/
+
+    contactButton.addEventListener("click", () => {
+        contactModal.classList.add("active");
+        document.body.classList.add("no-scroll");
+    });
+
+    buttonCloseModal.addEventListener("click", () => {
+        contactModal.classList.remove("active");
+    });
+
+    contactModal.addEventListener("click", (event) => {
+        const modalContent = document.querySelector('[data-modal-content]')
+        if(!modalContent.contains(event.target)) {
+            contactModal.classList.remove("active");
+        }
+    });
+
+}
